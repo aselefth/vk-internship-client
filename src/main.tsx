4b37a9ceeb7ba523c4d3608ddfffa426654fbac2
aselefth <andrei200287@gmail.com>
2023-06-12 19:11:35 +0300

    changed basepath for router

--- src/main.tsx
+++ src/main.tsx
@@ -1,86 +1,89 @@
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import store from './store/store.ts';
-import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import store from "./store/store.ts";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 import {
-	UpdateUserPage,
-	ProtecredRoute,
-	FeedPage,
-	AccountPage,
-	SignInPage,
-	SignUpPage,
-	RootLayout,
-	UserLikedPage,
-	UserPostsPage,
-	SubsFeedPage,
-	SubscribersPage,
-	SubscriptionsPage
-} from './index.ts';
+   UpdateUserPage,
+   ProtecredRoute,
+   FeedPage,
+   AccountPage,
+   SignInPage,
+   SignUpPage,
+   RootLayout,
+   UserLikedPage,
+   UserPostsPage,
+   SubsFeedPage,
+   SubscribersPage,
+   SubscriptionsPage
+} from "./index.ts";
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <RootLayout />,
-		children: [
-			{
-				path: '/',
-				element: <ProtecredRoute />,
-				children: [
-					{
-						path: '/:id',
-						element: <AccountPage />,
-						children: [
-							{
-								path: '/:id/posts',
-								element: <UserPostsPage />
-							},
-							{
-								path: '/:id/liked',
-								element: <UserLikedPage />
-							}
-						]
-					},
-					{
-						path: '/:id/subscribers',
-						element: <SubscribersPage />
-					},
-					{
-						path: '/:id/subscriptions',
-						element: <SubscriptionsPage />
-					},
-					{
-						path: '/feed',
-						element: <FeedPage />
-					},
-					{
-						path: '/subscribed',
-						element: <SubsFeedPage />
-					},
-					{
-						path: '/update',
-						element: <UpdateUserPage />
-					},
-					
-				]
-			},
-			{
-				path: '/signin',
-				element: <SignInPage />
-			},
-			{
-				path: '/signup',
-				element: <SignUpPage />
-			}
-		]
-	}
-]);
+const routes = [
+   {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+         {
+            path: "/",
+            element: <ProtecredRoute />,
+            children: [
+               {
+                  path: "/:id",
+                  element: <AccountPage />,
+                  children: [
+                     {
+                        path: "/:id/posts",
+                        element: <UserPostsPage />
+                     },
+                     {
+                        path: "/:id/liked",
+                        element: <UserLikedPage />
+                     }
+                  ]
+               },
+               {
+                  path: "/:id/subscribers",
+                  element: <SubscribersPage />
+               },
+               {
+                  path: "/:id/subscriptions",
+                  element: <SubscriptionsPage />
+               },
+               {
+                  path: "/feed",
+                  element: <FeedPage />
+               },
+               {
+                  path: "/subscribed",
+                  element: <SubsFeedPage />
+               },
+               {
+                  path: "/update",
+                  element: <UpdateUserPage />
+               }
+            ]
+         },
+         {
+            path: "/signin",
+            element: <SignInPage />
+         },
+         {
+            path: "/signup",
+            element: <SignUpPage />
+         }
+      ]
+   }
+];
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<Provider store={store}>
-		<CookiesProvider>
-			<RouterProvider router={router} />
-		</CookiesProvider>
-	</Provider>
+const router = createBrowserRouter(routes, {
+   basename: import.meta.env.DEV ? "/" : "/vk-internship-client/"
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+   <Provider store={store}>
+      <CookiesProvider>
+         <RouterProvider router={router} />
+      </CookiesProvider>
+   </Provider>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		watch: {
-			usePolling: true
-		},
-		host: true,
-		strictPort: true
-	},
+export default defineConfig(({ command }) => {
+   const config = {
+      plugins: [react()],
+      server: {
+         port: 3000,
+         watch: {
+            usePolling: true
+         },
+         host: true,
+         strictPort: true
+      },
+      base: "/"
+   };
+
+   if (command !== "serve") {
+      config.base = "/vk-internship-client/";
+   }
+
+   return config;
 });
